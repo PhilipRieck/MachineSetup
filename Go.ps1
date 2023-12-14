e82721a63546179f968555dfe6ee4192802c6815
Philip Rieck <me@philiprieck.com>
2023-12-14 09:55:47 -0600

    progressaction not in pwsh 3-5

--- Go.ps1
+++ Go.ps1
@@ -6,7 +6,7 @@ $TempDir = "$env:TEMP\MachineSetup"
 Remove-Item $TempDir -Recurse -Force -ErrorAction SilentlyContinue
 New-Item -Path $TempDir -ItemType Directory > $null
 $ZipPath = "$TempDir\main.zip"
-Invoke-WebRequest -Uri https://github.com/PhilipRieck/MachineSetup/archive/refs/heads/main.zip -OutFile $ZipPath -ProgressAction SilentlyContinue
+Invoke-WebRequest -Uri https://github.com/PhilipRieck/MachineSetup/archive/refs/heads/main.zip -OutFile $ZipPath
 Expand-Archive -LiteralPath $ZipPath -DestinationPath $TempDir
 $SetupScript = (Get-ChildItem -Path $TempDir -Filter BootstrapPwsh.ps1 -Recurse).FullName
 & $SetupScript @args
