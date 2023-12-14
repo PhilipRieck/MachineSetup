b2ce53bf8d2dfff97642a74e80a39684ca905447
Philip Rieck <me@philiprieck.com>
2023-12-14 08:55:05 -0600

    why is a store getting created!!

--- Parts/LtiSecrets.ps1
+++ Parts/LtiSecrets.ps1
@@ -1,10 +1,9 @@
 
-
 write-host "Checking for SecretManagement module"
 $module = Get-InstalledPsResource Microsoft.PowerShell.SecretManagement -ErrorAction Ignore
 if($module -eq $null){
     write-host "Secretmanagement [$module]. Installing."
-    Install-PSResource Microsoft.PowerShell.SecretManagement -TrustRepository
+    Install-PSResource Microsoft.PowerShell.SecretManagement, Microsoft.PowerShell.SecretStore -TrustRepository -Repository PSGallery
 }
 
 write-host "Checking for SecretStore module"
@@ -12,6 +11,15 @@ $module = Get-InstalledPsResource Microsoft.PowerShell.SecretStore -ErrorAction 
 if($module -eq $null){
     write-host "SecretStore [$module]. Installing."
     Install-PSResource Microsoft.PowerShell.SecretStore -TrustRepository -Confirm:$false
+}
+
+#Allow getting secrets from LtiSecrets vault without prompting
+write-host "Checking for Secretstore interaction settings"
+$config = Get-SecretStoreConfiguration
+if(($config.Interaction -ne "None")){
+    write-host "Interaction set to [$config.Interaction] -setting to None.  Auth set to [$config.Authentication] - setting to None."
+    # Note that there's actually only one SecretStore vault - it will duplicate the settings to all vaults for that module.
+    # See https://learn.microsoft.com/en-us/powershell/utility-modules/secretmanagement/how-to/manage-secretstore?view=ps-modules
     Set-SecretStoreConfiguration -Authentication None -Interaction None -Confirm:$false
 }
 
@@ -21,15 +29,3 @@ if($vault -eq $null){
     write-host "Vault [$module]. Registering."
     Register-SecretVault -Name LtiSecrets -ModuleName Microsoft.PowerShell.SecretStore
 }
-
-#Allow getting secrets from LtiSecrets vault without prompting
-write-host "Checking for Secretstore interaction settings"
-$config = Get-SecretStoreConfiguration
-if(($config.Interaction -ne "None")){
-    write-host "Interaction set to [$config.Interaction]. Setting to None.  Auth set to [$config.Authentication]."
-    # Note that there's actually only one SecretStore vault - it will duplicate the settings to all vaults for that module.
-    # See https://learn.microsoft.com/en-us/powershell/utility-modules/secretmanagement/how-to/manage-secretstore?view=ps-modules
-    Set-SecretStoreConfiguration -Authentication None -Interaction None -Confirm:$false
-}
-
-
