f6ec056f40bd166493f5f2d4ca0a56c50d2addf6
Philip Rieck <me@philiprieck.com>
2023-12-14 08:21:48 -0600

    fix progress on winget download

--- Parts/InstallWinget.ps1
+++ Parts/InstallWinget.ps1
@@ -5,9 +5,9 @@ $desiredModuleVersion = [semver]"1.6.3133"
 function installWinget(){
     write-host "Installing winget $desiredVersion"
     #Update winget to preview version
-    Invoke-WebRequest -Uri "https://github.com/microsoft/winget-cli/releases/download/v$($desiredVersion)/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle" -OutFile "$env:TEMP\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle"
-    Invoke-WebRequest -Uri "https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx" -OutFile "$env:TEMP\Microsoft.VCLibs.x64.14.00.Desktop.appx"
-    Invoke-WebRequest -Uri "https://github.com/microsoft/microsoft-ui-xaml/releases/download/v2.7.3/Microsoft.UI.Xaml.2.7.x64.appx" -OutFile "$env:TEMP\Microsoft.UI.Xaml.2.7.x64.appx"
+    Invoke-WebRequest -Uri "https://github.com/microsoft/winget-cli/releases/download/v$($desiredVersion)/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle" -OutFile "$env:TEMP\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle" -ProgressAction SilentlyContinue
+    Invoke-WebRequest -Uri "https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx" -OutFile "$env:TEMP\Microsoft.VCLibs.x64.14.00.Desktop.appx" -ProgressAction SilentlyContinue
+    Invoke-WebRequest -Uri "https://github.com/microsoft/microsoft-ui-xaml/releases/download/v2.7.3/Microsoft.UI.Xaml.2.7.x64.appx" -OutFile "$env:TEMP\Microsoft.UI.Xaml.2.7.x64.appx" -ProgressAction SilentlyContinue
 
     Add-AppxPackage "$env:TEMP\Microsoft.VCLibs.x64.14.00.Desktop.appx" -ErrorAction SilentlyContinue
     Add-AppxPackage "$env:TEMP\Microsoft.UI.Xaml.2.7.x64.appx" -ErrorAction SilentlyContinue
