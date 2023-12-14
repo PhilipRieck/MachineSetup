fb7e5fc88a4c43cd485a3842335629394d57649d
Philip Rieck <me@philiprieck.com>
2023-12-14 12:16:48 -0600

    wsl again

--- Parts/RancherDesktop.ps1
+++ Parts/RancherDesktop.ps1
@@ -4,22 +4,26 @@ Import-Module "$PSScriptRoot\..\Modules\Packages.psm1"
 $minimumVersion = [semver]"1.10.0"
 
 
-$hvState = (Get-WindowsOptionalFeature -FeatureName "Microsoft-Hyper-V" -Online | Select-Object -ExpandProperty State)
-if($hvState -ne "Enabled"){
-    Write-Host "`tHyper-V not installed. Enabling Hyper-V (will require a reboot)"
-    Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All -NoRestart
-    bcdedit /set hypervisorlaunchtype auto
-    SetRebootRequired
-    return
-} else {
-    Write-Host "`tHyper-V already enabled."
-}
+# NOTE - if you're running this in a VM, you'll need to run the following ON THE HOST, while the VM is shut down:
+#Set-VmProcessor -ExposeVirtualizationExtensions $true -VMName <VMName>
+
+# $hvState = (Get-WindowsOptionalFeature -FeatureName "Microsoft-Hyper-V" -Online | Select-Object -ExpandProperty State)
+# if($hvState -ne "Enabled"){
+#     Write-Host "`tHyper-V not installed. Enabling Hyper-V (will require a reboot)"
+#     Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All -NoRestart
+#     bcdedit /set hypervisorlaunchtype auto
+#     SetRebootRequired
+#     return
+# } else {
+#     Write-Host "`tHyper-V already enabled."
+# }
 
 #Rancher desktop requires WSL
 #Install WSL if required
 $wslState = (Get-WindowsOptionalFeature -FeatureName "Microsoft-Windows-Subsystem-Linux" -Online | Select-Object -ExpandProperty State)
 if($wslState -ne "Enabled"){
     Write-Host "`tWSL not installed. Enabling WSL (may require a reboot)"
+    bcdedit /set hypervisorlaunchtype auto
     wsl --install --no-launch
     SetRebootRequired
     return
